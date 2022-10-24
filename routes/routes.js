34c4ff5736ca669da4bbb83519f21dece51610b0
Sai kumar Silaparasetti <saikumarsilaparasetti@Sais-MacBook-Air.local>
2022-10-24 09:44:20 +0530

    alert added

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 const router = require('express').Router()
 const shortUrl = require('../models/shortUrl');
+const alert = require('alert')
 
-router.get('/', async (req, res)=>{
+router.get('*', async (req, res)=>{
+    if(req.params[0] === '/already_exists')
+        alert('User already exists')
     const shortUrls = await shortUrl.find();
 
     res.render('index', {shortUrls: shortUrls})
@@ -10,9 +13,13 @@ router.get('/', async (req, res)=>{
 
 
 router.post('/shortUrls', async (req, res)=>{
-    const newRecord =  await shortUrl.create({full: req.body.fullUrl});
-    if(newRecord ==  null)res.status(501).send('Record not added')
-    res.redirect('/')
+    const url = await shortUrl.findOne({full: req.body.fullUrl});
+    if(url != null){
+        res.redirect('/already_exists')
+    }else{
+        const newRecord =  await shortUrl.create({full: req.body.fullUrl});
+        res.redirect('/')
+    }
 })
 
 router.get('/:shortUrl', async (req, res)=>{
